300e6fee396a93c84fb1313750345d0cc52b9af8
Danil Perevalov <d.perevalov@ltst.ru>
2015-05-25 22:31:44 +0600

    #6 New design for SharingDialog. #3 Resize images on container size. #7 Create broadcast for add image in Gallery

--- app/src/main/java/com/ozm/rocks/ui/main/MyCollectionAdapter.java
+++ app/src/main/java/com/ozm/rocks/ui/main/MyCollectionAdapter.java
@@ -1,14 +1,21 @@
 package com.ozm.rocks.ui.main;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.net.Uri;
 import android.view.View;
 
+import com.facebook.common.references.CloseableReference;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.bitmaps.PlatformBitmapFactory;
+import com.facebook.imagepipeline.request.BasePostprocessor;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.facebook.imagepipeline.request.Postprocessor;
 import com.ozm.R;
 import com.ozm.rocks.data.api.response.ImageResponse;
 import com.ozm.rocks.ui.misc.ListBindableAdapter;
@@ -29,20 +36,52 @@ public class MyCollectionAdapter extends ListBindableAdapter<ImageResponse> {
 
     @Override
     public void bindView(ImageResponse item, int position, View view) {
+        final float halfXScreenSize = mDisplaySize.x;
         SimpleDraweeView mImageView = (SimpleDraweeView) view.findViewById(R.id.my_collection_grid_view_item);
         mImageView.setAspectRatio(item.width / (float) item.height);
         Uri uri = Uri.parse(item.url);
+        Postprocessor redMeshPostprocessor = new BasePostprocessor() {
+            @Override
+            public String getName() {
+                return "redMeshPostprocessor";
+            }
+
+            @Override
+            public CloseableReference<Bitmap> process(
+                    Bitmap sourceBitmap,
+                    PlatformBitmapFactory bitmapFactory) {
+                float ratio = (float) sourceBitmap.getWidth() / halfXScreenSize;
+                CloseableReference<Bitmap> bitmapRef = bitmapFactory.createBitmap(
+                        (int) (sourceBitmap.getWidth() / ratio),
+                        (int) (sourceBitmap.getHeight() / ratio));
+                try {
+                    Bitmap destBitmap = bitmapRef.get();
+                    Bitmap tempBitmap = Bitmap.createScaledBitmap(sourceBitmap, (int) (sourceBitmap.getWidth() / ratio),
+                            (int) (sourceBitmap.getHeight() / ratio), false);
+                    for (int x = 0; x < destBitmap.getWidth(); x++) {
+                        for (int y = 0; y < destBitmap.getHeight(); y++) {
+                            destBitmap.setPixel(x, y, tempBitmap.getPixel(x, y));
+                        }
+                    }
+                    return CloseableReference.cloneOrNull(bitmapRef);
+                } finally {
+                    CloseableReference.closeSafely(bitmapRef);
+                }
+            }
+        };
+
+        ImageRequest request = ImageRequestBuilder.newBuilderWithSource(uri)
+                .setPostprocessor(redMeshPostprocessor)
+                .build();
+
         if (item.mainColor != null) {
             mImageView.setBackgroundColor(Color.parseColor("#" + item.mainColor));
         }
-        if (item.isGIF) {
-            DraweeController controller = Fresco.newDraweeControllerBuilder()
-                    .setUri(uri)
-                    .setAutoPlayAnimations(true)
-                    .build();
-            mImageView.setController(controller);
-        } else {
-            mImageView.setImageURI(uri);
-        }
+        DraweeController controller = Fresco.newDraweeControllerBuilder()
+                .setUri(uri)
+                .setAutoPlayAnimations(true)
+                .setImageRequest(request)
+                .build();
+        mImageView.setController(controller);
     }
 }
