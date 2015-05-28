8be952a18de6a481adaf52542e18c2862adeaac8
Danil Perevalov <d.perevalov@ltst.ru>
2015-05-28 17:44:38 +0600

    #6 hide topCantainer in sharingDialog if 0 packages

--- app/src/main/java/com/ozm/rocks/ui/misc/Misc.java
+++ app/src/main/java/com/ozm/rocks/ui/misc/Misc.java
@@ -1,7 +1,21 @@
 package com.ozm.rocks.ui.misc;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+
+import com.ozm.R;
+import com.ozm.rocks.util.PInfo;
+
 /**
  * Created by Danil on 28.05.2015.
  */
 public class Misc {
+    public static Drawable getDrawable(int resource, Resources resources){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return resources.getDrawable(resource, null);
+        } else {
+            return resources.getDrawable(resource);
+        }
+    }
 }
