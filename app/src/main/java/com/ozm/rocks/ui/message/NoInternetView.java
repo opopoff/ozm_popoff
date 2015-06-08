8932b9d3bfd3103073ebd9666f72783ebdcbef42
Danil Perevalov <d.perevalov@ltst.ru>
2015-06-08 19:29:16 +0600

    #Bugs restyle no internet message

--- app/src/main/java/com/ozm/rocks/ui/message/NoInternetView.java
+++ app/src/main/java/com/ozm/rocks/ui/message/NoInternetView.java
@@ -2,12 +2,13 @@ package com.ozm.rocks.ui.message;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 /**
  * Created by Danil on 04.06.2015.
  */
-public class NoInternetView extends LinearLayout {
+public class NoInternetView extends FrameLayout {
     public NoInternetView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
