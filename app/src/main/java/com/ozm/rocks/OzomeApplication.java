b9435805cc273f68278ae00e08fe2279107e6f60
Danil Perevalov <d.perevalov@ltst.ru>
2015-06-15 14:19:02 +0600

    #Bugs change typefont

--- app/src/main/java/com/ozm/rocks/OzomeApplication.java
+++ app/src/main/java/com/ozm/rocks/OzomeApplication.java
@@ -36,7 +36,9 @@ public class OzomeApplication extends Application {
 //                        build()
 //        );
 //
-        CustomTypeface.getInstance().registerTypeface("roboto", getAssets(), "fonts/roboto_regular.ttf");
+        CustomTypeface.getInstance().registerTypeface("regular", getAssets(), "fonts/roboto_regular.ttf");
+        CustomTypeface.getInstance().registerTypeface("light", getAssets(), "fonts/roboto_light.ttf");
+        CustomTypeface.getInstance().registerTypeface("medium", getAssets(), "fonts/roboto_medium.ttf");
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
             refWatcher = RefWatcher.DISABLED;
