d136edb1462077d2cc08fe2de83b5f861a458d31
Danil Perevalov <d.perevalov@ltst.ru>
2015-07-15 17:07:52 +0600

    #Bugs change widget and pooshwoosh icons

--- app/src/main/java/com/ozm/rocks/ui/widget/WidgetController.java
+++ app/src/main/java/com/ozm/rocks/ui/widget/WidgetController.java
@@ -62,7 +62,7 @@ public class WidgetController {
                         .setColor(context.getResources().getColor(R.color.primary))
 //                        .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
 //                                R.drawable.ic_widget_big))
-                        .setSmallIcon(R.drawable.ic_widget)
+                        .setSmallIcon(R.drawable.widget_icon)
                         .setPriority(NotificationCompat.PRIORITY_MAX);
 //                        .setVisibility(NotificationCompat.VISIBILITY_SECRET);
 //                        .setContentTitle(context.getString(R.string.widget_title))
