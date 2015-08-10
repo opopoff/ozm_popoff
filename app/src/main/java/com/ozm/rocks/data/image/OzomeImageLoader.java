9ffcbfe37b027e536620d816ec4a2b127f593e44
Danil Perevalov <d.perevalov@ltst.ru>
2015-08-10 19:04:29 +0600

    #Bugs Fetching gifs

--- app/src/main/java/com/ozm/rocks/data/image/OzomeImageLoader.java
+++ app/src/main/java/com/ozm/rocks/data/image/OzomeImageLoader.java
@@ -8,6 +8,8 @@ import android.widget.ImageView;
 
 import com.koushikdutta.ion.Ion;
 import com.ozm.rocks.ApplicationScope;
+import com.ozm.rocks.data.FileService;
+import com.ozm.rocks.data.TokenStorage;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.thin.downloadmanager.DownloadRequest;
@@ -28,6 +30,8 @@ import timber.log.Timber;
 @ApplicationScope
 public class OzomeImageLoader {
 
+    @Inject
+    TokenStorage tokenStorage;
     public static final int IMAGE = 1;
     public static final int GIF = 2;
 
@@ -41,19 +45,21 @@ public class OzomeImageLoader {
     private final Picasso picasso;
     private ThinDownloadManager downloadManager;
     private static final int DOWNLOAD_THREAD_POOL_SIZE = 4;
-    private final Ion ion;
+//    private final Ion ion;
 
     @Inject
     public OzomeImageLoader(Application application, Ion ion, Picasso picasso) {
         this.context = application.getApplicationContext();
         this.picasso = picasso;
-        this.ion = ion;
+//        this.ion = ion;
         downloadManager = new ThinDownloadManager(DOWNLOAD_THREAD_POOL_SIZE);
     }
 
     public void fetch(@Type int type, String url) {
         if (type == IMAGE) {
             picasso.load(url).fetch();
+        } else if (type == GIF) {
+            loadGif(url, null);
         }
     }
 
@@ -65,55 +71,7 @@ public class OzomeImageLoader {
     public void load(@Type int type, final String url, ImageView target, final Listener listener) {
         target.setImageDrawable(null);
         if (type == GIF) {
-            Uri downloadUri = Uri.parse(url);
-            final String path = context.getExternalCacheDir()
-                    + url.substring(url.lastIndexOf(File.separator) + 1, url.length()) + "_tumb";
-            File file = new File(path);
-            if (file.exists()) {
-                if (listener != null) {
-                    listener.onSuccess(getByteArrayFromFile(path));
-                }
-            } else {
-                Uri destinationUri = Uri.parse(path);
-                final DownloadRequest downloadRequest = new DownloadRequest(downloadUri)
-                        .setDestinationURI(destinationUri).setPriority(DownloadRequest.Priority.HIGH)
-                        .setDownloadListener(new DownloadStatusListener() {
-                            @Override
-                            public void onDownloadComplete(int id) {
-                                if (listener != null) {
-                                    listener.onSuccess(getByteArrayFromFile(path));
-                                }
-                            }
-
-                            @Override
-
-                            public void onDownloadFailed(int id, int errorCode, String errorMessage) {
-                                if (listener != null) {
-                                    listener.onError();
-                                }
-                            }
-
-                            @Override
-                            public void onProgress(int id, long totalBytes, long downloadedBytes, int progress) {
-
-                            }
-                        });
-                downloadManager.add(downloadRequest);
-            }
-//            Ion.with(context).load(url).asByteArray().setCallback(new FutureCallback<byte[]>() {
-//                @Override
-//                public void onCompleted(Exception e, byte[] bytes) {
-//                    if (e == null) {
-//                        if (listener != null) {
-//                            listener.onSuccess(bytes);
-//                        }
-//                    } else {
-//                        if (listener != null) {
-//                            listener.onError();
-//                        }
-//                    }
-//                }
-//            });
+            loadGif(url, listener);
         } else {
             picasso.load(url).noFade().into(target, new Callback() {
                         @Override
@@ -131,6 +89,47 @@ public class OzomeImageLoader {
                         }
                     }
             );
+        }
+    }
+
+    public void loadGif(String url, final Listener listener) {
+        Uri downloadUri = Uri.parse(url);
+//        final String path = context.getExternalCacheDir()
+//                + url.substring(url.lastIndexOf(File.separator) + 1, url.length()) + "_tumb";
+        final String path = FileService.getFullFileName(context, url, "gif", tokenStorage.isCreateAlbum(), false);
+        File file = new File(path);
+        if (file.exists()) {
+            if (listener != null) {
+                Timber.d("OzomeImageLoader gif: exist");
+                listener.onSuccess(getByteArrayFromFile(path));
+            }
+        } else {
+            Uri destinationUri = Uri.parse(path);
+            final DownloadRequest downloadRequest = new DownloadRequest(downloadUri)
+                    .setDestinationURI(destinationUri).setPriority(DownloadRequest.Priority.HIGH)
+                    .setDownloadListener(new DownloadStatusListener() {
+                        @Override
+                        public void onDownloadComplete(int id) {
+                            if (listener != null) {
+                                Timber.d("OzomeImageLoader gif: download");
+                                listener.onSuccess(getByteArrayFromFile(path));
+                            }
+                        }
+
+                        @Override
+
+                        public void onDownloadFailed(int id, int errorCode, String errorMessage) {
+                            if (listener != null) {
+                                listener.onError();
+                            }
+                        }
+
+                        @Override
+                        public void onProgress(int id, long totalBytes, long downloadedBytes, int progress) {
+
+                        }
+                    });
+            downloadManager.add(downloadRequest);
         }
     }
 
