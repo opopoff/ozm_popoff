31e48ca5786d58531c1d37c53ee756eef47392a6
Aleksandr Mirko <cosicsmile@gmail.com>
2015-09-01 13:38:01 +0600

    #Change end point url for rest request on server

--- app/src/internalDebug/java/com/ozm/rocks/data/api/ApiEndpoint.java
+++ app/src/internalDebug/java/com/ozm/rocks/data/api/ApiEndpoint.java
@@ -1,6 +1,6 @@
 package com.ozm.rocks.data.api;
 
 public class ApiEndpoint {
-    public static final String PRODUCTION_API_URL = "http://ozm.rocks:49124";
-    public static final String INTERNAL_API_URL = "http://debug.ozm.rocks:49124";
+    public static final String PRODUCTION_API_URL = "http://ozm.io";
+    public static final String INTERNAL_API_URL = "http://debug.ozm.io";
 }
--- app/src/production/java/com/ozm/rocks/data/api/ApiEndpoint.java
+++ app/src/production/java/com/ozm/rocks/data/api/ApiEndpoint.java
@@ -1,5 +1,5 @@
 package com.ozm.rocks.data.api;
 
 public class ApiEndpoint {
-    public static final String PRODUCTION_API_URL = "http://ozm.rocks:49124";
+    public static final String PRODUCTION_API_URL = "http://ozm.io";
 }
