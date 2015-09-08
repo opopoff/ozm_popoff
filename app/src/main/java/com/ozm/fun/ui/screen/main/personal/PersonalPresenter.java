a2be1090a5fa94b0395ee0423663e8ca293072ec
Danil Perevalov <d.perevalov@ltst.ru>
2015-09-08 18:30:10 +0600

    #Remove onbording on Golde screen and Personal screen

--- app/src/main/java/com/ozm/fun/ui/screen/main/personal/PersonalPresenter.java
+++ app/src/main/java/com/ozm/fun/ui/screen/main/personal/PersonalPresenter.java
@@ -63,18 +63,19 @@ public final class PersonalPresenter extends BasePresenter<PersonalView> {
             reloadFeed();
         }
 
-        onBoardingDialogBuilder.setCallBack(new OnBoardingDialogBuilder.ChooseDialogCallBack() {
-            @Override
-            public void apply() {
-                tokenStorage.setCreateAlbum(true);
-                localyticsController.showAlbumOnBoarding(LocalyticsController.CREATE);
-            }
+//        onBoardingDialogBuilder.setCallBack(new OnBoardingDialogBuilder.ChooseDialogCallBack() {
+//            @Override
+//            public void apply() {
+//                tokenStorage.setCreateAlbum(true);
+//                localyticsController.showAlbumOnBoarding(LocalyticsController.CREATE);
+//            }
+//
+//            @Override
+//            public void cancel() {
+//                localyticsController.showAlbumOnBoarding(LocalyticsController.SKIP);
+//            }
+//        });
 
-            @Override
-            public void cancel() {
-                localyticsController.showAlbumOnBoarding(LocalyticsController.SKIP);
-            }
-        });
     }
 
     public void openOnBoardingDialog() {
